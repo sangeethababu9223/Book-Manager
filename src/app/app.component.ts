26ad07a0c1d24da51ecac5731616223eea0a701b
Nila <sangeetha9223@gmail.com>
2020-07-24 13:21:31 +0530

    Navigation restriction based on logged in or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'book-list';
+  loggedIn;
+  constructor(
+    private authService : AuthService,
+    ) { }
+
+    ngOnInit(): void {
+      this.loggedIn = this.authService.isAuthenticated()
+    }
 }
